974a1f149a1e13747885e182a61851a53e38e416
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-10-17 15:30:28 +0300

    database connectivity added Closes #2

--- istif_backend/src/main/java/com/599/istif_backend/IstifBackendApplication.java
+++ istif_backend/src/main/java/com/599/istif_backend/IstifBackendApplication.java
@@ -1,4 +1,4 @@
-package com9.istif_backend;
+package com.istif_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- istif_backend/src/test/java/com/599/istif_backend/IstifBackendApplicationTests.java
+++ istif_backend/src/test/java/com/599/istif_backend/IstifBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com9.istif_backend;
+package com.istif_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
