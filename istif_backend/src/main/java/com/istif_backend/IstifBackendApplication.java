0f916c2343c5cc50896f9d7c5ea1a6d58bfefcc9
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-10-17 14:47:52 +0300

    initial commit for backend. Closes #1

---
+++ istif_backend/src/main/java/com/istif_backend/IstifBackendApplication.java
@@ -0,0 +1,12 @@
+package com9.istif_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstifBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IstifBackendApplication.class, args);
+	}
+
+}
