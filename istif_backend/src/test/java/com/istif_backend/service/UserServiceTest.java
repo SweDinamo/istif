37894ab73ef2f4d9494e4bcbc9b8503e2155c9e0
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2023-11-26 20:53:55 +0300

    unit test capability added

---
+++ istif_backend/src/test/java/com/istif_backend/service/UserServiceTest.java
@@ -0,0 +1,64 @@
+package com.istif_backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void getTokenFromEndpoint() {
+    }
+
+    @Test
+    void validateTokenizedUser() {
+    }
+
+    @Test
+    void findByUserToken() {
+    }
+
+    @Test
+    void findByUserId() {
+    }
+
+    @Test
+    void findByUsernameAndPassword() {
+    }
+
+    @Test
+    void generateUserToken() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void findByEmailAndPassword() {
+    }
+
+    @Test
+    void findByIdentifierAndPassword() {
+    }
+
+    @Test
+    void followUser() {
+    }
+
+    @Test
+    void editUser() {
+    }
+
+    @Test
+    void updateUserPhoto() {
+    }
+}
